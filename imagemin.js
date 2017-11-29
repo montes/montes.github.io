7115946359d494cdc60584b6786422a7950c687b
montes <jmontes@incubalia.com>
2017-11-29 23:49:54 +0100

    Still a bit more jpgs quality

--- imagemin.js
+++ imagemin.js
@@ -5,8 +5,8 @@ imagemin(['assets/images-src/*.jpg'], 'assets/images', {
 	plugins: [
 		imageminJpegRecompress({'quality': 'high',
 			'accurate': true,
-			'min': 65,
-			'max': 80})
+			'min': 70,
+			'max': 85})
 	]
 }).then(() => {
 	console.log('Images optimized');
