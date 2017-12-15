152a8339ae1c0b4bd5d18ae1803d02c8d0816fce
montes <jmontes@incubalia.com>
2017-12-15 21:02:38 +0100

    Upgraded imagemin.js to optimize also png files

--- imagemin.js
+++ imagemin.js
@@ -1,7 +1,7 @@
 const imagemin = require('imagemin');
 const imageminJpegRecompress = require('imagemin-jpeg-recompress');
 
-imagemin(['assets/images-src/*.jpg'], 'assets/images', {
+imagemin(['assets/images-src/*.jpg', 'assets/images-src/*.png'], 'assets/images', {
 	plugins: [
 		imageminJpegRecompress({'quality': 'high',
 			'accurate': true,
